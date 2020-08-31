e56b071c295909ed5af05604121f9585d684bace
antoine <antoinemayol@yahoo.com>
2020-08-31 11:03:38 +0200

    Login finished

--- ID/get_login_informations.py
+++ ID/get_login_informations.py
@@ -5,13 +5,14 @@ def get_login(bot):
     username_to_send = username
     password_to_send = password
 
+    instagram_password_minimum_size = 6
+    password_too_small = True
+
     #Tant que nom d'utilisateur est vide
-    while username_to_send == "":
+    while username_to_send == "" and not bot.username_good:
 
         #Aucune données n'est enregustré
         bot.log_not_save = True
-        password_too_small = True
-        instagram_password_minimum_size = 6
 
         #Demande du nom d'utilisateur si non correct ou non existant
         if not bot.username_good : username_to_send=input("Enter your Instagram login:")
@@ -24,11 +25,10 @@ def get_login(bot):
         if len(password_to_send) < instagram_password_minimum_size:print("Your password is too small, it must be bigger than "+str(instagram_password_minimum_size)+".")
         #Si le mot de passe est asser grand passer à la suite
         elif len(password_to_send) >= instagram_password_minimum_size:password_too_small = False
+
+    if bot.username != "": username_to_send = bot.username
     return username_to_send,password_to_send
 
 #Débeugage
 if __name__ == "__main__":
-    from login_informations import username,password
-    from main import IG_BOT
-    Bot = IG_BOT()
-    get_login()
+    print("Le débeugage ne marche pas ici")
--- main.py
+++ main.py
@@ -8,9 +8,10 @@ class IG_BOT():
         
         self.start_driver()
 
+        self.username_good,self.password_good = False,False
+        self.username, self.password ="",""
         while not self.account_login:
             self.log_not_save = False
-            self.username_good,self.password_good = False,False
             self.username, self.password = get_login(self)
             self.login()
         #verify files
@@ -26,29 +27,28 @@ class IG_BOT():
             print("Something went wrong verify your driver files")
         
     def login(self):
-        '''
-            Login:
-            If username wrong do something
-        '''
         self.driver.get("https://www.instagram.com/accounts/login/")
         sleep(1)
         self.driver.find_element_by_xpath('//input[@name="username"]').send_keys(self.username)
         self.driver.find_element_by_xpath('//input[@name="password"]').send_keys(self.password)
         self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         url_get = False
+        
         while not url_get:
             try:
-                text_error = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div[2]')
+                text_error = self.driver.find_element_by_xpath('//p[contains(text(), "' + "Le nom d’utilisateur entré n’appartient à aucun compte. Veuillez le vérifier et réessayer." + '")]')
                 url_get = True
                 print("Your username is wrong ! ;)")
             except:
-                text_error = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div[2]')
-                url_get = True
-                print("Your password is wrong ! ;)")
-                self.username_good = True
-            finally:
-                if self.driver.current_url == "https://www.instagram.com/":
+                try:
+                    text_error = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div[2]')
                     url_get = True
-                    self.account_login,self.username_good,self.password_good = True,True,True
+                    print("Your password is wrong ! ;)")
+                    self.username_good = True
+                except:
+                    if self.driver.current_url == "https://www.instagram.com/accounts/onetap/?next=%2F":
+                        url_get = True
+        
+        self.account_login,self.username_good,self.password_good = True,True,True
                     
 if __name__ == "__main__":IG_BOT()
